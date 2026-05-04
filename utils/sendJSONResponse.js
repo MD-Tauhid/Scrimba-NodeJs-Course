81cf83f7d3531bbce41e10c992c9f5f3c5cd5c91
MD-Tauhid <tauhid.swe@gmail.com>
2026-05-04 10:39:26 +0600

    handle cors error solution

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,9 @@
 export const sendJSONResponse = (res, status, payload) => {
     res.setHeader("Content-Type", "application/json");
+
+    // to avoid cors error in frontend application running on different port
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
     res.statusCode = status;
     res.end(JSON.stringify(payload));
 };
